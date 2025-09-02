0c270c8e295e8a9cf452a033a8444df111357159
breaze <crack9810@gmail.com>
2025-09-02 18:38:49 -0500

    Adding comments

--- tcp_server/src/main/java/org/breaze/Main.java
+++ tcp_server/src/main/java/org/breaze/Main.java
@@ -3,6 +3,9 @@ package org.breaze;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    /** Main class to start the TCP server.
+     * Configures SSL properties and initiates the server on the specified port.
+     */
     public static void main(String[] args) {
         TCPServer server = new TCPServer(2020);
         server.start();
